4cb5eca1211860b535f0195390a6201a2ad5cdae
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-02-04 10:42:28 +0900

    ベンチマークを整理。

--- bench.lua
+++ bench.lua
@@ -1,9 +1,11 @@
 #! /usr/bin/env lua
 
-local driver = require "impl2"
 local pure = require "dromozoa.utf8.pure"
+local difftime = os.difftime
+local time = os.time
+local unpack = table.unpack or unpack
 
-local data = table.concat {
+local utf8_char = table.concat {
   string.char(0x41, 0xE2, 0x89, 0xA2, 0xCE, 0x91, 0x2E);
   string.char(0xED, 0x95, 0x9C, 0xEA, 0xB5, 0xAD, 0xEC, 0x96, 0xB4);
   string.char(0xE6, 0x97, 0xA5, 0xE6, 0x9C, 0xAC, 0xE8, 0xAA, 0x9E);
@@ -17,10 +19,6 @@ local codepoint = {
   0xFEFF, 0x0233B4,
 }
 
-local time = os.time
-local difftime = os.difftime
-local unpack = table.unpack or unpack
-
 local result, posix_sys_time = pcall(require, "posix.sys.time")
 if result then
   time = posix_sys_time.gettimeofday
@@ -32,47 +30,52 @@ if result then
       return sec + (t2.tv_usec - t1.tv_usec) * 0.000001
     end
   end
-else
-  print(posix_sys_time)
 end
 
-local n = 100000
--- local n = 1
+local n = tonumber(arg[1]) or 100000
 
-local name = { "native", "pure", "decode1", "decode2", "decode3", "decode4", "decode5" }
-for j = 1, #name do
-  local x = 0
-  local utf8_len = driver(name[j])
+local function bench(name, fn)
   local t1 = time()
-  for i = 1, n do
-    x = x + utf8_len(data)
-  end
+  fn(utf8[name], n)
   local t2 = time()
-  print(name[j], difftime(t2, t1), x)
+  local t = difftime(t2, t1)
+
+  local t1 = time()
+  fn(pure[name], n)
+  local t2 = time()
+  local u = difftime(t2, t1)
+
+  print(name, t, u, u / t)
 end
 
-local codes = { utf8.codes, pure.codes }
-for i = 1, #codes do
+print("name", "utf8", "pure")
+
+bench("char", function (fn, n)
   local x = 0
-  local fn = codes[i]
-  local t1 = time()
-  for j = 1, n do
-    for p, c in fn(data) do
+  for i = 1, n do
+    x = x + select("#", fn(unpack(codepoint)))
+  end
+end)
+
+bench("codes", function (fn, n)
+  local x = 0
+  for i = 1, n do
+    for p, c in fn(utf8_char) do
       x = x + 1
     end
   end
-  local t2 = time()
-  print(difftime(t2, t1), x)
-end
+end)
 
-local char = { utf8.char, pure.char }
-for i = 1, #char do
+bench("len", function (fn, n)
   local x = 0
-  local fn = char[i]
-  local t1 = time()
-  for j = 1, n do
-    x = x + select("#", fn(unpack(codepoint)))
+  for i = 1, n do
+    x = x + fn(utf8_char)
   end
-  local t2 = time()
-  print(difftime(t2, t1), x)
-end
+end)
+
+bench("offset", function (fn, n)
+  local x = 0
+  for i = 1, n do
+    x = x + fn(utf8_char, -1)
+  end
+end)
