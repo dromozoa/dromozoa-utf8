b4dbe04c4c36060e35dd04787d93ed421409d390
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-11-30 09:18:58 +0900

    decodeの実装を追加。

--- experimental/decode4.lua
+++ experimental/decode4.lua
@@ -79,17 +79,49 @@ end
 
 local byte = string.byte
 
+local A, B, C, D, E, F, G
+local I
+local N
+
 return function (s, i)
-  local j = i + 3
-  local a, b, c, d = byte(s, i, j)
+  if i == 1 then
+    A, B, C, D = byte(s, 1, 4)
+    E = nil
+    F = nil
+    G = nil
+    I = 5
+    N = #s
+  else
+    if I <= N then
+      if not A then
+        A, B, C, D = byte(s, I, I + 3)
+        I = I + 4
+      elseif not B then
+        B, C, D, E = byte(s, I, I + 3)
+        I = I + 4
+      elseif not C then
+        C, D, E, F = byte(s, I, I + 3)
+        I = I + 4
+      elseif not D then
+        D, E, F, G = byte(s, I, I + 3)
+        I = I + 4
+      end
+    end
+  end
+
+  -- io.stderr:write(("%8d %8d %02X %02X %02X %02X %02X %02X %02X\n"):format(I, N, A or 0, B or 0, C or 0, D or 0, E or 0, F or 0, G or 0))
+
+  local a, b, c, d = A, B, C, D
   local x = H1[a]
   if x then
     if a <= 0xDF then
       if a <= 0x7F then
+        A, B, C, D, E, F, G = B, C, D, E, F, G, nil
         return i + 1, x
       else
         local b = TA_80_BF[b]
         if b then
+          A, B, C, D, E, F, G = C, D, E, F, G, nil, nil
           return i + 2, x + b
         end
       end
@@ -98,13 +130,15 @@ return function (s, i)
         local b = H2[a][b]
         local c = TA_80_BF[c]
         if b and c then
-          return j, x + b + c
+          A, B, C, D, E, F, G = D, E, F, G, nil, nil, nil
+          return i + 3, x + b + c
         end
       else
         local b = H2[a][b]
         local c = TB_80_BF[c]
         local d = TA_80_BF[d]
         if b and c and d then
+          A, B, C, D, E, F, G = E, F, G, nil, nil, nil, nil
           return i + 4, x + b + c + d
         end
       end
