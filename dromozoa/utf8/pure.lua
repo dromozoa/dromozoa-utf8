4d602f949a1f0ea0d42d419eb7a90b84cadd70f2
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-12-01 12:27:03 +0900

    リファクタリング。

--- dromozoa/utf8/pure.lua
+++ dromozoa/utf8/pure.lua
@@ -15,93 +15,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-utf8.  If not, see <http://www.gnu.org/licenses/>.
 
-local char = string.char
-local concat = table.concat
-local floor = math.floor
-local select = select
-local unpack = table.unpack or unpack
 local decode = require "dromozoa.utf8.decode"
 local encode = require "dromozoa.utf8.encode"
 
-local function _encode(a)
-  if a < 0 then
-    return nil
-  elseif a <= 0x7F then
-    return char(a)
-  elseif a <= 0x07FF then
-    local b = a % 0x40
-    local a = floor(a / 0x40)
-    return char(a + 0xC0, b + 0x80)
-  elseif a <= 0xFFFF then
-    if 0xD800 <= a and a <= 0xDFFF then return nil end
-    local c = a % 0x40
-    local a = floor(a / 0x40)
-    local b = a % 0x40
-    local a = floor(a / 0x40)
-    return char(a + 0xE0, b + 0x80, c + 0x80)
-  elseif a <= 0x10FFFF then
-    local d = a % 0x40
-    local a = floor(a / 0x40)
-    local c = a % 0x40
-    local a = floor(a / 0x40)
-    local b = a % 0x40
-    local a = floor(a / 0x40)
-    return char(a + 0xF0, b + 0x80, c + 0x80, d + 0x80)
-  else
-    return nil
-  end
-end
-
-local function _decode(s, i)
-  local a, b, c, d = s:byte(i, i + 3)
-  if a == nil then
-    return nil
-  elseif a <= 0x7F then
-    return i + 1, a
-  elseif 0xC2 <= a then
-    if a <= 0xDF then
-      if b == nil or b < 0x80 or 0xBF < b then return nil end
-      local a = a % 0x20 * 0x40
-      local b = b % 0x40
-      return i + 2, a + b
-    elseif a <= 0xEF then
-      if a <= 0xEC then
-        if a == 0xE0 then
-          if b == nil or b < 0xA0 or 0xBF < b then return nil end
-        else
-          if b == nil or b < 0x80 or 0xBF < b then return nil end
-        end
-      else
-        if a == 0xED then
-          if b == nil or b < 0x80 or 0x9F < b then return nil end
-        else
-          if b == nil or b < 0x80 or 0xBF < b then return nil end
-        end
-      end
-      if c == nil or c < 0x80 or 0xBF < c then return nil end
-      local a = a % 0x10 * 0x1000
-      local b = b % 0x40 * 0x40
-      local c = c % 0x40
-      return i + 3, a + b + c
-    elseif a <= 0xF4 then
-      if a == 0xF0 then
-        if b == nil or b < 0x90 or 0xBF < b then return nil end
-      elseif a <= 0xF3 then
-        if b == nil or b < 0x80 or 0xBF < b then return nil end
-      else
-        if b == nil or b < 0x80 or 0x8F < b then return nil end
-      end
-      if c == nil or c < 0x80 or 0xBF < c then return nil end
-      if d == nil or d < 0x80 or 0xBF < d then return nil end
-      local a = a % 0x08 * 0x040000
-      local b = b % 0x40 * 0x1000
-      local c = c % 0x40 * 0x40
-      local d = d % 0x40
-      return i + 4, a + b + c + d
-    end
-  end
-  return nil
-end
+local concat = table.concat
+local select = select
+local unpack = table.unpack or unpack
 
 local function char(...)
   local result = {}
