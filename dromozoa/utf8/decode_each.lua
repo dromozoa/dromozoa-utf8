de67951eb141c281ddbbaa2fa7f1e1e96c4801b8
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-23 10:02:10 +0900

    著作権表記を更新。

--- dromozoa/utf8/decode_each.lua
+++ dromozoa/utf8/decode_each.lua
@@ -9,11 +9,11 @@
 --
 -- dromozoa-utf8 is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with dromozoa-utf8.  If not, see <http://www.gnu.org/licenses/>.
+-- along with dromozoa-utf8. If not, see <https://www.gnu.org/licenses/>.
 
 local check_string = require "dromozoa.utf8.check_string"
 local decode_table = require "dromozoa.utf8.decode_table"
